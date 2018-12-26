f5b1293cb9922c02173b5acd21d50785020996aa
Amin Bahrami <abp.bahrami@gmail.com>
2018-12-26 11:23:48 +0330

    Add upload file supporting

--- ABPWebService/src/main/java/com/aminbahrami/abpwebservice/ABPWebService.java
+++ ABPWebService/src/main/java/com/aminbahrami/abpwebservice/ABPWebService.java
@@ -1,9 +1,13 @@
 package com.aminbahrami.abpwebservice;
 
 import android.os.Handler;
+import android.util.Base64;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -45,17 +49,36 @@ public class ABPWebService
 		this.iOnNetwork=iOnNetwork;
 	}
 	
-	public void sendRequest(final String inputName,final String message)
+	public void sendRequest(String data)
 	{
-		Log.i("ABPWebService","Request: "+message);
+		sendRequest(null,data);
+	}
+	
+	public void sendRequest(String inputName,String data)
+	{
+		sendRequest(inputName,data,null,null);
+	}
+	
+	public void sendRequest(final String inputName,final String data,final String fileInputName,final File file)
+	{
+		Log.i("ABPWebService","Request: "+data);
 		
 		Thread thread=new Thread(new Runnable()
 		{
 			@Override
 			public void run()
 			{
-				//String requestData=inputName+"="+message;
-				String requestData=message;
+				String requestData;
+				
+				if(inputName==null)
+				{
+					requestData=data;
+				}
+				else
+				{
+					requestData=inputName+"="+data;
+				}
+				
 				
 				URL apiUrl=null;
 				
@@ -68,15 +91,72 @@ public class ABPWebService
 					httpURLConnection.setConnectTimeout(connectTimeout);
 					httpURLConnection.setReadTimeout(readTimeout);
 					
-					httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+					if(inputName==null)
+					{
+						httpURLConnection.setRequestProperty("Content-Type","application/json; charset=UTF-8");
+					}
 					
 					httpURLConnection.setRequestMethod("POST");
 					httpURLConnection.setDoInput(true);
 					httpURLConnection.setDoOutput(true);
 					
-					OutputStreamWriter osw=new OutputStreamWriter(httpURLConnection.getOutputStream(),"UTF-8");
-					osw.write(requestData);
-					osw.flush();
+					DataOutputStream dataOutputStream=null;
+					
+					if(file!=null)
+					{
+						String boundary="***************";
+						
+						String lineEnd="\r\n";
+						String twoHyphens="--";
+						
+						FileInputStream fileInputStream=new FileInputStream(file);
+						httpURLConnection.setUseCaches(false);
+						httpURLConnection.setRequestProperty("ENCTYPE","multipart/form-data");
+						httpURLConnection.setRequestProperty("Content-Type","multipart/form-data;boundary="+boundary);
+						
+						
+						String content="Content-Disposition: form-data; name=\""+inputName+"\""+lineEnd+lineEnd+data+lineEnd+lineEnd;
+						content+=twoHyphens+boundary+lineEnd+"Content-Disposition: form-data; name=\""+fileInputName+"\";filename=\""+file.getName()+"\""+lineEnd;
+						
+						dataOutputStream=new DataOutputStream(httpURLConnection.getOutputStream());
+						dataOutputStream.writeBytes(twoHyphens+boundary+lineEnd);
+						dataOutputStream.writeBytes(content);
+						dataOutputStream.writeBytes(lineEnd);
+						
+						int byteAvailable, bufferSize, bytesRead;
+						byte[] buffer;
+						int maxBufferSize=1024*1024;
+						
+						byteAvailable=fileInputStream.available();
+						bufferSize=Math.min(byteAvailable,maxBufferSize);
+						
+						buffer=new byte[bufferSize];
+						bytesRead=fileInputStream.read(buffer,0,bufferSize);
+						
+						while(bytesRead>0)
+						{
+							dataOutputStream.write(buffer,0,bufferSize);
+							byteAvailable=fileInputStream.available();
+							bufferSize=Math.min(byteAvailable,maxBufferSize);
+							
+							bytesRead=fileInputStream.read(buffer,0,bufferSize);
+						}
+						
+						dataOutputStream.writeBytes(lineEnd);
+						dataOutputStream.writeBytes(twoHyphens+boundary+twoHyphens+lineEnd);
+					}
+					else
+					{
+						OutputStreamWriter osw=new OutputStreamWriter(httpURLConnection.getOutputStream(),"UTF-8");
+						osw.write(requestData);
+						osw.flush();
+					}
+					
+					if(dataOutputStream!=null)
+					{
+						dataOutputStream.close();
+						dataOutputStream.flush();
+					}
 					
 					BufferedReader br=new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
 					
@@ -91,7 +171,10 @@ public class ABPWebService
 					
 					Log.i("ABPWebService","Response: "+strAll);
 					
-					iOnNetwork.onResponse(strAll.toString());
+					if(iOnNetwork!=null)
+					{
+						iOnNetwork.onResponse(strAll.toString());
+					}
 				}
 				catch(MalformedURLException e)
 				{
--- app/src/main/java/com/aminbahrami/abpwebservicelib/ActivityMain.java
+++ app/src/main/java/com/aminbahrami/abpwebservicelib/ActivityMain.java
@@ -2,8 +2,16 @@ package com.aminbahrami.abpwebservicelib;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 
 import com.aminbahrami.abpwebservice.ABPWebService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 
 public class ActivityMain extends Activity
 {
@@ -13,5 +21,38 @@ public class ActivityMain extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.sendRequest).setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				sendRequest();
+			}
+		});
+	}
+	
+	private void sendRequest()
+	{
+		ABPWebService abpWebService=new ABPWebService();
+		abpWebService.setUrl("http://192.168.1.2/test/androidUploadFile/upload.php");
+		
+		//Check Permission in the feature
+		File file=new File(Environment.getExternalStorageDirectory()+"/test.jpg");
+		
+		Log.i("LOG","FilePath: "+file);
+		
+		JSONObject object=new JSONObject();
+		try
+		{
+			object.put("test","Hello");
+			object.put("age",28);
+		}
+		catch(JSONException e)
+		{
+			e.printStackTrace();
+		}
+		
+		abpWebService.sendRequest(object.toString());
 	}
 }
